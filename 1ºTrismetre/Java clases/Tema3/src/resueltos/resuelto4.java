60e8e8b969ecdaeeb90f64fb8c7123f2c1448499
Javier González Giménez <114144556+gonzgimnzjavier@users.noreply.github.com>
2022-10-28 07:34:18 +0200

    subo resuelto

--- 1ºTrismetre/Java clases/Tema3/src/resueltos/resuelto4.java
+++ 1ºTrismetre/Java clases/Tema3/src/resueltos/resuelto4.java
@@ -2,37 +2,30 @@ package resueltos;
 
 import java.util.Scanner;
 
-/*- 4. Realiza un método que, dado un número de tres cifras, averigüe si es un número Armstrong. Un número es
-Armstrong cuando la suma de cada uno de los números que lo componen elevado al número de dígitos de dicho
-número da como resultado el propio número. Como esta definición es algo compleja, con la siguiente imagen se verá
-más claro qué es un número Armstrong:*/
-
 public class resuelto4 {
+
+public static void main(String[] args) {
 	
-	public static void main(String[] args) {
-		
-		int num=0;
-		int suma=0;
-		
-		Scanner sc = new Scanner (System.in);
-		System.out.println("dime un numero de TRES cifras: ");
-		num =sc.nextInt();
-		int centena;
-		int decena;
-		int unidad;
-		
-		centena=num/100;			// 153 / 100 --->1
-		decena= (num%100)/10;		// 53  / 10  --->5
-		unidad= (num%100)%10;		// 53  % 10 ->>>>3
-		
-		//System.out.println("la centena es: "+centena +" la decena es: "+decena+" y la unidad es: "+unidad);
-		suma= (int) (Math.pow(centena, 3.) +Math.pow(decena, 3) +Math.pow(unidad, 3));
-		
-		if(num==suma) {
-			System.out.println(num+ " es armstrong");
-		}else {
-			System.out.println("no es armstrong");
-		}
-	}
+	Scanner sc = new Scanner (System.in);
+	System.out.println("introduce numero:");
+	int numero= sc.nextInt();
+	
+	int milesima;
+	int centesima;
+	int decima;
+	int unidad; 
+	
+	milesima= numero/1000;
+	centesima= ((numero%1000)/100);
+	decima= (((numero%1000)%100)/10);
+	unidad= (((numero%1000)%100)%10);
+	
+	int armstrong=  (int) ((Math.pow(milesima, 4)) + (Math.pow(centesima, 4)) + (Math.pow(decima, 4)) + (Math.pow(unidad, 4)));
+	
+	if (numero == armstrong) {
+		System.out.println("Es armstrong");
+	}else {System.out.println("no es armstrong");}
+	
+}
 
 }
